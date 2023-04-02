63029a580beb4ce932dd1b0aa9f2fd2a741f4e39
Jhenbert Villamucho <jvv205756@gmail.com>
2023-04-02 13:58:34 +0800

    scafolding of project

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+
+const Form = () => {
+  return <Fragment>enter form components here</Fragment>;
+};
+
+export default Form;
---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return <Fragment>About components here</Fragment>;
+};
+
+export default About;
