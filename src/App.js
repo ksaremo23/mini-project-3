511736f2e5a598a4f71d68cb7eefa3c2eb111556
Rizaldy Britanico <pluk.rizaldybritanico@gmail.com>
2023-04-03 18:17:47 +0800

    added files and components but subject to change upon review and qa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import About from './pages/About';
+//import User from './pages/User';
+import Drawer from './pages/Drawer';
+import React, { useState } from "react";
+//import ReactDOM from 'react-dom';
+import Header from './pages/Header';
+import SignIn from './pages/SignIn';
+//import SignUp from './pages/SignUp';
+//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleLogin = () => {
+    // code to handle successful login
+    setIsDrawerOpen(true);
+  };
+
   return (
-    <div className="App">
-      Test
+    <div>
+    <Header/>
+    <About/>
+      <button onClick={handleLogin}>Log In</button>
+      <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        {/* Drawer content */}
+      </Drawer>
+      <SignIn />
     </div>
   );
 }
 
-export default App;
+export default App
