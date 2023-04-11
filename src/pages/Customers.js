220592fcd87c56f1c0541ad5a83fc2adf6a9cd55
Rizaldy Britanico <pluk.rizaldybritanico@gmail.com>
2023-04-11 16:38:42 +0800

    added ViewCustomers

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,11 +1,64 @@
 import React, { Fragment } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ViewCustomers from '../components/ViewCustomers';
 
-const Customers = () => {
+
+function createData(customer_id, firstname, lastname, address, city, zip, email, phone) {
+  return { customer_id, firstname, lastname, address, city, zip, email, phone};
+}
+
+const rows = [
+  createData(),
+  createData(),
+  createData(),
+  createData(),
+  createData(),
+];
+
+export default function BasicTable() {
   return (
-    <Fragment>
-      <h1>This is Customers Page</h1>
-    </Fragment>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>customer-id</TableCell>
+            <TableCell align="right">firstname</TableCell>
+            <TableCell align="right">lastname</TableCell>
+            <TableCell align="right">address</TableCell>
+            <TableCell align="right">city</TableCell>
+            <TableCell align="right">zip</TableCell>
+            <TableCell align="right">email</TableCell>
+            <TableCell align="right">phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.customer_id}</TableCell>
+              <TableCell align="right">{row.firstame}</TableCell>
+              <TableCell align="right">{row.lastname}</TableCell>
+              <TableCell align="right">{row.address}</TableCell>
+              <TableCell align="right">{row.city}</TableCell>
+              <TableCell align="right">{row.zip}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-};
+}
 
-export default Customers;
