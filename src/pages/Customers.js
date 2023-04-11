a428e3b54a5345f57ad9e25b064275b9147ca2cf
Rizaldy Britanico <pluk.rizaldybritanico@gmail.com>
2023-04-11 17:01:35 +0800

    updated Customers

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,64 +1,89 @@
-import React, { Fragment } from "react";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 import ViewCustomers from '../components/ViewCustomers';
 
+const columns = [
+  { field: 'customer_id', headerName: 'Customer_ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'Address',
+    headerName: 'Address',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
 
-function createData(customer_id, firstname, lastname, address, city, zip, email, phone) {
-  return { customer_id, firstname, lastname, address, city, zip, email, phone};
-}
-
-const rows = [
-  createData(),
-  createData(),
-  createData(),
-  createData(),
-  createData(),
+  {
+    field: 'City',
+    headerName: 'City',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'Zip',
+    headerName: 'Zip',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'Email',
+    headerName: 'Email',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'Phone',
+    headerName: 'Phone',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
 ];
 
-export default function BasicTable() {
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789' },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789'  },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789' },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789' },
+  { id: 6, lastName: 'Melisandre', firstName: 'Dennis', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789' },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789' },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789'  },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', address: 'Manila', city: 'Taguig', zip: '4511', email: 'sample@email.com', phone: '123456789' },
+];
+
+export default function DataGridDemo() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>customer-id</TableCell>
-            <TableCell align="right">firstname</TableCell>
-            <TableCell align="right">lastname</TableCell>
-            <TableCell align="right">address</TableCell>
-            <TableCell align="right">city</TableCell>
-            <TableCell align="right">zip</TableCell>
-            <TableCell align="right">email</TableCell>
-            <TableCell align="right">phone</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.customer_id}</TableCell>
-              <TableCell align="right">{row.firstame}</TableCell>
-              <TableCell align="right">{row.lastname}</TableCell>
-              <TableCell align="right">{row.address}</TableCell>
-              <TableCell align="right">{row.city}</TableCell>
-              <TableCell align="right">{row.zip}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.phone}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
   );
 }
-
